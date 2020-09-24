c5b9a56e0e33c91e733e5a4e2e2835290c8fc5cd
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-09-25 03:05:31 +0530

    updated

--- ass1.py
+++ ass1.py
@@ -1,46 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
-class airfoil:
 
-    def __init__(self, airfoil_name="NACA0012", airfoil_type="symmetric", resolution=10):
+
+class Airfoil:
+
+    def __init__(self, airfoil_name="NACA0012", airfoil_type="symmetric", resolution=10, alpha = 10):
         self.type = airfoil_type
         self.name = airfoil_name
         self.resolution = resolution
+        self.alpha = np.deg2rad(alpha)
 
     def name_scrape(self):
         return self.name[4], self.name[5], self.name[6], self.name[7]
-
 
     def construct_airfoil(self):
         if self.type == "symmetric":
             theta = np.linspace(0, np.pi, 50)
             x_c = 0.5 * (1 - np.cos(theta))
-            a, b, c, d =  self.name_scrape()
+            x_c = np.flip(x_c) # flipped so as per fotran program
+            a, b, c, d = self.name_scrape()
             t = int(c+d)/100
             top = 5*t*(0.2969*(x_c**0.5) - 0.1260*x_c - 0.3516*(x_c**2) + 0.2843*(x_c**3) - 0.1015*(x_c**4))
             bot = - top
             top[-1] = 0
             x_c_new = np.append(x_c, np.flip(x_c)[1:])
-            foil = np.append(top, np.flip(bot)[1:])
-            return foil, x_c_new
+            self.foil = np.append(top, np.flip(bot)[1:])
+            self.x_c = x_c_new
+            print(self.x_c)
+            pass
 
     def vortices(self):
-        foil, x_c_new  = self.construct_airfoil()
-        x_vort = (x_c_new[1:] + x_c_new[0:-1]) * 0.5
-        y_vort = (foil[1:] + foil[0:-1]) * 0.5
-        return x_vort, y_vort
+        self.construct_airfoil()
+        self.x_vort = (self.x_c[1:] + self.x_c[0:-1]) * 0.5
+        self.y_vort = (self.foil[1:] + self.foil[0:-1]) * 0.5
+        self.length = (self.foil[1:] - self.foil[0:-1]) ** 2 + (self.x_c[1:] - self.x_c[0:-1]) ** 2
+        pass
+
+    def angles(self):
+        self.theta = np.arctan2(self.foil[1:] - self.foil[0:-1], self.x_c[1:], self.x_c[0:-1])
+        self.cosine = np.cos(self.theta)
+        self.sine = np.sin(self.theta)
+
+
+    def solve(self):
+        self.RHS = np.sin(self.theta - self.alpha)
+
+
 
     def plot(self):
-        foil, x_c = self.construct_airfoil()
-        x_vort, y_vort = self.vortices()
-        plt.plot(x_c, foil, "*--", x_vort, y_vort, "*r")
+        self.construct_airfoil()
+        self.vortices()
+        plt.plot(self.x_c, self.foil, "*--", self.x_vort, self.y_vort, "*r")
         plt.show()
 
 
-
-
 if __name__ == "__main__":
-    a = airfoil()
+    a = Airfoil()
     a.vortices()
     a.plot()
 
