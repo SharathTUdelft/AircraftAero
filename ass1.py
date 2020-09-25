156e99eba331023ad8dd133a38cd7b0e1744c7fe
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-09-25 06:26:03 +0530

    need to debug

--- ass1.py
+++ ass1.py
@@ -4,16 +4,25 @@ import matplotlib.pyplot as plt
 
 class Airfoil:
 
-    def __init__(self, airfoil_name="NACA0012", airfoil_type="symmetric", resolution=10, alpha = 10):
+    def __init__(self, airfoil_name="NACA0012", airfoil_type="symmetric", resolution=10, alpha = 8):
         self.type = airfoil_type
         self.name = airfoil_name
         self.resolution = resolution
         self.alpha = np.deg2rad(alpha)
-        self.panels = self.resolution -1
+
+        self.construct_airfoil()
+
         self.cn1 = np.zeros((self.panels, self.panels))
         self.cn2 = np.zeros((self.panels, self.panels))
         self.ct1 = np.zeros((self.panels, self.panels))
         self.ct2 = np.zeros((self.panels, self.panels))
+        self.AT = np.zeros((self.panels, self.points))
+        self.AN = np.zeros((self.points, self.points))
+        self.gamma = np.zeros((self.points, 1))
+        self.RHS = np.zeros((self.points, 1))
+        self.V = np.zeros(self.panels)
+        self.CP = np.zeros(self.panels)
+        print("fsd")
 
     def name_scrape(self):
         return self.name[4], self.name[5], self.name[6], self.name[7]
@@ -31,7 +40,8 @@ class Airfoil:
             x_c_new = np.append(x_c, np.flip(x_c)[1:])
             self.foil = np.append(top, np.flip(bot)[1:])
             self.x_c = x_c_new
-            print(self.x_c)
+            self.points = len(self.foil)
+            self.panels = self.points - 1
             pass
 
     def vortices(self):
@@ -39,16 +49,18 @@ class Airfoil:
         self.x_vort = (self.x_c[1:] + self.x_c[0:-1]) * 0.5
         self.y_vort = (self.foil[1:] + self.foil[0:-1]) * 0.5
         self.length = (self.foil[1:] - self.foil[0:-1]) ** 2 + (self.x_c[1:] - self.x_c[0:-1]) ** 2
-        pass
 
     def angles(self):
         self.theta = np.arctan2(self.foil[1:] - self.foil[0:-1], self.x_c[1:], self.x_c[0:-1])
         self.cosine = np.cos(self.theta)
         self.sine = np.sin(self.theta)
 
-
     def solve(self):
-        self.RHS = np.sin(self.theta - self.alpha)
+        self.construct_airfoil()
+        self.vortices()
+        self.angles()
+        for i in range(self.panels):
+            self.RHS[i] = self.theta[i] - self.alpha
 
         for i in range(self.panels):
             for j in range(self.panels):
@@ -72,7 +84,37 @@ class Airfoil:
                         ((self.y_vort[i] - self.foil[j]) * np.sin(self.theta[i] - 2 * self.theta[j]))
 
                     self.cn2[i, j] = D + (0.5 * Q * F/self.length[j]) - (A*C + D*E) * G/self.length[j]
-                    self.cn1[i, j] =
+                    self.cn1[i, j] = 0.5 * D * F + C*G - self.cn2[i, j]
+                    self.ct2[i, j] = C + 0.5*P*F/self.length[j] + (A*D - C*E) * G/self.length[j]
+                    self.ct1[i, j] = 0.5*C*F - D*G - self.ct2[i, j]
+
+        for i in range(self.panels):
+            self.AN[i, 0] = self.cn1[i, 0]
+            self.AN[i, -1] = self.cn2[i, -1]
+
+            self.AT[i, 0] = self.ct1[i, 0]
+            self.AT[i, -1] = self.ct2[i, -1]
+
+            for j in range(1, self.panels):
+                self.AN[i, j] = self.cn1[i, j] + self.cn2[i, j-1]
+                self.AT[i, j] = self.ct1[i, j] + self.ct2[i, j-1]
+
+        self.AN[-1, 0] = 1
+        self.AN[-1, -1] = 1
+        for j in range(1, self.panels):
+            self.AN[-1, j] = 0
+
+        print(self.AN.shape)
+        self.gamma = np.linalg.solve(self.AN, self.RHS)
+        print(self.gamma)
+
+        for i in range(self.panels):
+            self.V[i] = np.cos(self.theta[i] - self.alpha)
+            for j in range(self.points):
+                self.V[i] = self.V[i] + self.AT[i, j]*self.gamma[j]
+                self.CP[i] = 1 - self.V[i]**2
+
+
 
     def plot(self):
         self.construct_airfoil()
@@ -83,6 +125,6 @@ class Airfoil:
 
 if __name__ == "__main__":
     a = Airfoil()
-    a.vortices()
-    a.plot()
+
+    a.solve()
 
