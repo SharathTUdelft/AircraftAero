453e15ba813ba261837c2817a5bb9238a1dc0804
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-10-08 23:49:20 +0530

    initial commit of declarations file

---
+++ declarations.py
@@ -0,0 +1,25 @@
+import numpy as np
+from airfoil import AirfoilGen
+
+airfoilcons = AirfoilGen(airfoil_name="NACA0012", chord=1, nb_points=8)
+airfoilcons.run()
+x_c = airfoilcons.x_foil
+foil = airfoilcons.y_foil
+
+foil[0] = 0
+foil[-1] = 0
+
+panels = airfoilcons.panels
+nb_points = panels + 1
+alpha = np.deg2rad(5)
+cn1 = np.zeros((panels,panels))
+cn2 = np.zeros((panels,panels))
+ct1 = np.zeros((panels,panels))
+ct2 = np.zeros((panels,panels))
+AT = np.zeros((panels, nb_points))
+AN = np.zeros((nb_points, nb_points))
+gamma = np.zeros((nb_points, 1))
+RHS = np.zeros((nb_points, 1))
+V = np.zeros(panels)
+CP = np.zeros(panels)
+
